e993f3f47cd2558b090e08dd0f1b2ba904851c0d
Reid <einsub@gmail.com>
2016-05-11 19:12:12 +0900

    miss-spell correction

--- ios/RNSegment.m
+++ ios/RNSegment.m
@@ -1,5 +1,5 @@
 #import "RNSegment.h"
-#import "SEGAnalyics.h"
+#import "SEGAnalytics.h"
 
 @implementation RNSegment {
 }
@@ -9,24 +9,25 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(setup:(NSString* )writeKey)
 {
     SEGAnalyticsConfiguration *configuration = [SEGAnalyticsConfiguration configurationWithWriteKey:writeKey];
-    configuration.flushAt = flushAt;
+    //configuration.flushAt = flushAt;
     configuration.shouldUseLocationServices = NO;
-    [SEGAnalytics setupWithConfiguration:[SEGAnalyticsConfiguration]];
+    [SEGAnalytics setupWithConfiguration:configuration];
 }
 
 RCT_EXPORT_METHOD(identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options)
 {
-    [[SEGAnalytics sharedAnalytics] identify:userId traits options];
+  [[SEGAnalytics sharedAnalytics] identify:userId traits:traits options:options];
 }
 
 RCT_EXPORT_METHOD(screen:(NSString *)name properties:(NSDictionary *)properties options:(NSDictionary *)options)
 {
-    [[SEGAnalytics sharedAnalytics] screen:name properties options];
+  [[SEGAnalytics sharedAnalytics] screen:name properties:properties options:options];
 }
 
 RCT_EXPORT_METHOD(track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options)
 {
-    [[SEGAnalytics sharedAnalytics] track:event properties options];
+  [[SEGAnalytics sharedAnalytics] track:event properties:properties options:options];
 }
 
 @end
+
